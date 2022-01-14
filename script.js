f575836a4f8c03ea21575260c4e9d6612c05f07c
antoniom28 <antomarc@gmail.com>
2022-01-14 16:32:13 +0100

    bonus n.1 + bonus re size + sistemate le funzioni

--- script.js
+++ script.js
@@ -1,87 +1,97 @@
 const images = [
     {
-        image : 'img/01.jpg',
-        title : 'Svezia',
-        text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     },
     {
-        image : 'img/02.jpg',
-        title : 'Svizzera',
-        text :'Lorem ipsum',
+        image: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum',
     },
     {
-        image : 'img/03.jpg',
-        title : 'Gran Bretagna',
-        text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     },
     {
-        image : 'img/04.jpg',
-        title : 'Germania',
-        text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     },
     {
-        image : 'img/05.jpg',
-        title : 'Paradise',
-        text :'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        image: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     },
 ];
 
 const min = 0;
 const max = images.length;
 let startImage = 0;
-imageAutoChange = setInterval(changeImageDown,3000);
+imageAutoChange = setInterval(changeImageButton, 3000);
 
 var app = new Vue(
     {
-        el:"#container",
-        data:{
-            mainTitle : images[startImage].title,
-            mainText : images[startImage].text,
-            mainImage : `url(${images[startImage].image})`,
-            imagePreview : images.map(x => `url(${x.image})`),
-            imageStatus : ["selected","","","",""],
+        el: "#container",
+        data: {
+            mainTitle: images[startImage].title,
+            mainText: images[startImage].text,
+            mainImage: `url(${images[startImage].image})`,
+            size: "normal",
+            imagePreview: images.map(x => `url(${x.image})`),
+            imageStatus: ["selected", "", "", "", ""],
         },
-        methods:{
-            autoChange : imageAutoChange,
-            down : changeImageDown,
-            up : changeImageUp,
-            reSize : function(){
-                this.className += 'ciao';
-                console.log('resizeee');
-            }
+        methods: {
+            autoChange: imageAutoChange,
+            down: changeImageButton,
+            up: changeImageButton,
+            reSize: function () {
+                if (this.size == "normal")
+                    this.size = 'big';
+                else
+                    this.size = "normal"
+            },
+            changeImageOnClick: changeImageClick,
         },
     }
 );
 
-function changeImageDown(){
+function changeImageButton(direction) {
     const elemento = app.$data;
     elemento.imageStatus[startImage] = ""; //rimuove lo stato selected dalla precedente immagine
-    startImage++;
-    if(startImage == max)
-        startImage = min;
-    elemento.imageStatus[startImage] = "selected"; //inseriamo selected alla nuova immagine principale
-    console.log(startImage,max);
-    elemento.mainImage = `url(${images[startImage].image})`;
-    elemento.mainTitle = images[startImage].title;
-    elemento.mainText = images[startImage].text;
+    if (direction == 'up') {
+        if (startImage == min)
+            startImage = max;
+        startImage--;
+    } else { // down
+        startImage++;
+        if (startImage == max)
+            startImage = min;
+    }
+
+    changeImage(elemento, startImage);
 }
 
-function changeImageUp(){
+function changeImageClick(index) {
     const elemento = app.$data;
-    elemento.imageStatus[startImage] = ""; //rimuove lo stato selected dalla precedente immagine
-    if(startImage == min)
-        startImage = max;
-    startImage--;
-    elemento.imageStatus[startImage] = "selected"; //inseriamo selected alla nuova immagine principale
-    console.log(startImage,images.length);
-    elemento.mainImage = `url(${images[startImage].image})`;
-    elemento.mainTitle = images[startImage].title;
-    elemento.mainText = images[startImage].text;
+    startImage = index;
+    for (let i = 0; i < max; i++)
+        elemento.imageStatus[i] = ""; //rimuove lo stato selected da tutte
+
+    changeImage(elemento, startImage);
 }
 
-document.getElementById('carousel').addEventListener('mouseenter',() =>{
+function changeImage(elemento, indice) {
+    elemento.imageStatus[indice] = "selected"; //inseriamo selected alla nuova immagine principale
+    elemento.mainImage = `url(${images[indice].image})`;
+    elemento.mainTitle = images[indice].title;
+    elemento.mainText = images[indice].text;
+}
+
+document.getElementById('carousel').addEventListener('mouseenter', () => {
     clearInterval(imageAutoChange);
 });
-document.getElementById('carousel').addEventListener('mouseleave',() =>{
-    imageAutoChange = setInterval(changeImageDown,3000);
+document.getElementById('carousel').addEventListener('mouseleave', () => {
+    imageAutoChange = setInterval(changeImageButton, 3000);
 });
